f18a255e7c874020fa81b0b1b5d84d42f42556c0
vince <vince@forevernine.com>
2023-03-02 21:59:25 +0800

    [feat] output

--- output/hello_zoe.service.worker.js
+++ output/hello_zoe.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1677695426|2782660958";
+const CACHE_VERSION = "1677765346|94044253";
 const CACHE_PREFIX = "hello_zoe-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "hello_zoe.offline.html";
